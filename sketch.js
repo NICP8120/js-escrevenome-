5948279d1fd206d06fc096cbcb8edc59182d1cca
NICP8120 <00001133452140SP@al.educacao.sp.gov.br>
2024-08-19 07:41:58 -0300

    adicionando arquivos js e css

--- sketch.js
+++ sketch.js
@@ -1,16 +1,15 @@
 function setup() {
-    createCanvas(500, 300
-              );
-    background("black")
+  createCanvas(500, 500
+            );
+  background("black")
+}
+
+function draw() {
+  stroke("blue");
+  fill("red");
+  
+
+  if (mouseIsPressed) {
+    rect(mouseX, mouseY, 20, 35);
   }
-  
-  function draw() {
-    stroke("blue");
-    fill("red");
-    
-  
-    if (mouseIsPressed) {
-      rect(mouseX, mouseY, 20, 35);
-    }
-  }
-  
+}
